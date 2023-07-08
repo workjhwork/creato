ebc2cd661183e7126cae36cb65d113fd1c77b718
Ducky <138562723+workjhwork@users.noreply.github.com>
2023-07-09 00:05:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
 const spinWheel = document.getElementById("spinWheel");
 const spinBtn = document.getElementById("spin_btn");
 const text = document.getElementById("text");
-Chart.defaults.font.family = "Verdana";
 /* --------------- Minimum And Maximum Angle For A value  --------------------- */
 const spinValues = [
-  { minDegree: 61, maxDegree: 90, value: 8 },
-  { minDegree: 31, maxDegree: 60, value: 3 },
-  { minDegree: 0, maxDegree: 30, value: "SPIN" },
-  { minDegree: 331, maxDegree: 360, value: 1 },
-  { minDegree: 301, maxDegree: 330, value: 38 },
-  { minDegree: 271, maxDegree: 300, value: 99 },
-  { minDegree: 241, maxDegree: 270, value: 50 },
-  { minDegree: 211, maxDegree: 240, value: 3 },
-  { minDegree: 181, maxDegree: 210, value: 18 },
-  { minDegree: 151, maxDegree: 180, value: 1 },
-  { minDegree: 121, maxDegree: 150, value: 50 },
-  { minDegree: 91, maxDegree: 120, value: 5 },
+  { minDegree: 61, maxDegree: 90, value: 100 },
+  { minDegree: 31, maxDegree: 60, value: 200 },
+  { minDegree: 0, maxDegree: 30, value: 300 },
+  { minDegree: 331, maxDegree: 360, value: 400 },
+  { minDegree: 301, maxDegree: 330, value: 500 },
+  { minDegree: 271, maxDegree: 300, value: 600 },
+  { minDegree: 241, maxDegree: 270, value: 700 },
+  { minDegree: 211, maxDegree: 240, value: 800 },
+  { minDegree: 181, maxDegree: 210, value: 900 },
+  { minDegree: 151, maxDegree: 180, value: 1000 },
+  { minDegree: 121, maxDegree: 150, value: 1100 },
+  { minDegree: 91, maxDegree: 120, value: 1200 },
 ];
 /* --------------- Size Of Each Piece  --------------------- */
 const size = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
@@ -37,29 +36,13 @@ var spinColors = [
   // ...
 ];
 
-var textColors = [
-  "#FF0000", // Bright red
-  "#FFFFFF", // White
-  "#FF0000", // Bright red
-  "#FFFFFF", // White
-  "#FF0000", // Bright red
-  "#FFFFFF", // White
-  "#FF0000", // Bright red
-  "#FFFFFF", // White
-  "#FF0000", // Bright red
-  "#FFFFFF", // White
-  "#FF0000", // Bright red
-  "#FFFFFF"  // White
-];
-
-
 /* --------------- Chart --------------------- */
 /* --------------- Guide : https://chartjs-plugin-datalabels.netlify.app/guide/getting-started.html --------------------- */
 let spinChart = new Chart(spinWheel, {
   plugins: [ChartDataLabels],
   type: "pie",
   data: {
-    labels: ["RM8", "RM3", "SPIN", "RM1", "RM38", "RM99", "RM50", "RM3", "RM18", "RM1", "RM50", "RM5"],
+    labels: ["$8", "$3", "SPIN", "$1", "$38", "$138", "$0.5", "$3", "$18", "$1", "$0.5", "$5"],
     datasets: [
       {
         backgroundColor: spinColors,
@@ -69,29 +52,22 @@ let spinChart = new Chart(spinWheel, {
   },
   options: {
     responsive: true,
-    animation: { duration: 1 },
+    animation: { duration: 0 },
     plugins: {
       tooltip: false,
       legend: {
         display: false,
       },
       datalabels: {
-        rotation: 90, // Set rotation to 0 to keep labels horizontal
+        rotation: 0, // Set rotation to 0 to keep labels horizontal
         anchor: 'center', // Position the labels at the center of each section
-        align: 'end', // Align the labels to the center of each section
-        color: textColors,
-        textShadowColor: "#ffd700",
-        textShadowBlur: 10,
-        textShadowOffsetX: 0,
-        textShadowOffsetY: 0,
+        align: 'center', // Align the labels to the center of each section
+        color: "#000000",
         formatter: (_, context) => context.chart.data.labels[context.dataIndex],
         font: {
-          // family: 'PT Serif, serif',
-          family: 'Roboto',
-          size: 14,
-          weight: 'normal',
-          style: 'italic',
-        },
+           size: 23, 
+           weight: 'bold',
+          }, // Decrease font size for better fit=
       },
     },
   },
@@ -101,7 +77,7 @@ let spinChart = new Chart(spinWheel, {
 const generateValue = (angleValue) => {
   for (let i of spinValues) {
     if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      // text.innerHTML = `<p>Congratulations, You Have Won RM${i.value} ! </p>`;
+      text.innerHTML = `<p>Congratulations, You Have Won $${i.value} ! </p>`;
       spinBtn.disabled = false;
       break;
     }
@@ -113,8 +89,7 @@ let resultValue = 101;
 spinBtn.addEventListener("click", () => {
   spinBtn.disabled = true;
   text.innerHTML = `<p>Best Of Luck!</p>`;
-  // let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
-  let randomDegree = Math.random() < 0.5 ? 39 : 230;
+  let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
   let rotationInterval = window.setInterval(() => {
     spinChart.options.rotation = spinChart.options.rotation + resultValue;
     spinChart.update();
